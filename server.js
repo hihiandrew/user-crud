9c97a1ed7e0a62e5d2c788a658afb647740665da
Andrew Reis <areis89@gmail.com>
2018-09-04 18:00:40 -0400

    deployable port added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { db, User } = require('./db');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
