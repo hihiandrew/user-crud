d39d8fcc52258ab51d3849cee12569dafcc7f46e
Andrew Reis <areis89@gmail.com>
2018-09-04 17:40:46 -0400

    pre-deploy

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <hr />
+      <p>Welcome to Acme Users! Try it out.</p>
+    </div>
+  );
+};
+
+export default Home;
